59c0e234ebfddddfbcf1d440a24e0cc806e9dc60
RuthuV96 <110454226+RuthuV96@users.noreply.github.com>
2022-11-02 15:41:47 +0100

    Changes navbar
    changes & experiments with navbar

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ async function typeWriterHeader() {
 function showNumber() {
     document.getElementById("number").innerHTML = txt2.charAt(0);
 }
+
+
+// Topbar stuff
+// Source: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
